34011bcb08b383cd23929fa792e03646593247ad
wzulfikar <>
2026-04-04 13:46:44 +0300

    add gh page deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://wzulfikar.com",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
